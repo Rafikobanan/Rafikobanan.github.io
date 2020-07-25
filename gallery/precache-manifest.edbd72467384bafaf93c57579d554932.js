c79d5c35bfc6c2de1f9b1dcd9716601e815c9782
Rafikobanan <58649618+Rafikobanan@users.noreply.github.com>
2020-07-25 11:36:31 +0300

    added gallery

---
+++ gallery/precache-manifest.edbd72467384bafaf93c57579d554932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2f3fb87f4391a97b48c59672c39f5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b887df6b71dccc15174c",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "6f7e91695adc782e157d",
+    "url": "/static/css/main.f39164ff.chunk.css"
+  },
+  {
+    "revision": "b887df6b71dccc15174c",
+    "url": "/static/js/2.52422f1d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.52422f1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7e91695adc782e157d",
+    "url": "/static/js/main.3e46f643.chunk.js"
+  },
+  {
+    "revision": "6df8defb7fac4f8ad5c4",
+    "url": "/static/js/runtime-main.02737e24.js"
+  }
+]);
