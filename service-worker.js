085e46287e9474b8cb7fdef8f648aab109ff6dc5
Rafikobanan <58649618+Rafikobanan@users.noreply.github.com>
2020-07-25 14:20:57 +0300

    updated gallery

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39e4c550d2431e771915ebadbc483410.js"
+  "/precache-manifest.5d5e3131118884c5a2d28d9ffb1fd590.js"
 );
 
 self.addEventListener('message', (event) => {
